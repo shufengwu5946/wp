a680e45dc55df281a0b6e0240917dd3199c3c9b8
shufengwu5946 <594695686@qq.com>
2019-03-05 00:10:53 +0800

    server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
-  element.innerHTML = 'hello wepack';
+  element.innerHTML = 'hello wepack demo';
 
   var btn = document.createElement('button');
   btn.innerHTML = 'Click me and check the console!';
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')

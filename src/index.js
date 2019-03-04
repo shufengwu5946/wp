17d50b864bf5616aab9f7a29d62d7429c2b0be93
shufeng.wu <shufeng.wu@deltaww.com>
2019-03-04 17:45:15 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import printMe from './print.js';
+
+function component() {
+  var element = document.createElement('div');
+  element.innerHTML = 'hello wepack';
+
+  var btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());

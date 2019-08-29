914e1c885bee83e9cb9a5d4e404e667b5b408c64
shufeng.wu <shufeng.wu@deltaww.com>
2019-08-29 14:16:58 +0800

    开发环境

--- devEnvironmentDemo/webpack.config.js
+++ devEnvironmentDemo/webpack.config.js
@@ -3,10 +3,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: { app: "./src/index.js", print: "./src/print.js" },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/'
   },
   plugins: [
     new CleanWebpackPlugin(),

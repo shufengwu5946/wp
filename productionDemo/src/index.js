8e52439da0871b574c988a5dce5e18daffe4efc1
shufeng.wu <shufeng.wu@deltaww.com>
2019-08-30 11:09:45 +0800

    生产环境

---
+++ productionDemo/src/index.js
@@ -0,0 +1,13 @@
+import { cube } from "./math.js";
+import "./style.css";
+
+function component() {
+  var element = document.createElement("pre");
+  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
+    "\n\n"
+  );
+  element.classList.add("fontColor");
+  return element;
+}
+
+document.body.appendChild(component());
